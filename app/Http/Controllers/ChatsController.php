0e2ed42f83722026aeea37f758103fbfbb758f67
Elliot Anderson <rafaelchirel@hotmail.com>
2019-06-18 08:41:03 -0400

    project

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use App\Events\MessageSent;
+use Illuminate\Http\Request;
+
+class ChatsController extends Controller
+{
+    public function __construct()
+    {
+    	$this->middleware('auth');
+    }
+
+    public function index()
+    {
+    	return view('chat');
+    }
+
+    public function fechMessages()
+    {
+    	 return Message::with('user')->get();
+    }
+
+    public function sendMessage(Request $request)
+    {
+    	$message = auth()->user()->messages()->create([
+    		'message' => $request->message
+    	]);
+
+    	broadcast(new MessageSent($message->load('user')))->toOthers();
+
+    	return ['status' => 'success'];
+    }
+}
